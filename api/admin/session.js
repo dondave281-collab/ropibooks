cccef02a1b4276e744c2b2e44cce12e141180e5f
dondave281-collab <dondave281@gmail.com>
2026-08-20 21:37:58 +0000

    Delete api directory

--- api/admin/session.js
+++
@@ -1,8 +0,0 @@
-const { ok, requireMethod, requireAdmin, withHandler } = require('../_lib/util');
-
-module.exports = withHandler(async (req, res) => {
-  if (!requireMethod(req, res, 'GET')) return;
-  const admin = await requireAdmin(req, res);
-  if (!admin) return;
-  ok(res, { username: admin.username });
-});
